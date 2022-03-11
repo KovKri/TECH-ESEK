1e18f9cb31b369f11328c882266e55af5105b908
KovKri <100101026+KovKri@users.noreply.github.com>
2022-03-11 01:02:40 +0100

    Add files via upload

--- Kovács Kristóf/Scripts/Game Menu/MainMenu.cs
+++ Kovács Kristóf/Scripts/Game Menu/MainMenu.cs
@@ -1,1 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //azert kell h jeleneteket tudjuk valtogatni
 
+public class MainMenu : MonoBehaviour
+{
+      public void PlayGame()
+      {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Indexeljuk a Scene -eket
+      }
+      public void QuitGame()
+      {
+        Debug.Log("QUIT!"); //ellenorzeshez kell
+        Application.Quit(); //kiléptet a jatekbol
+      }
+
+}
